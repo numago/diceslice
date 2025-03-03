386edc8e8fd8aa6eb64330194bd989edf4571af3
Fred Okoro <fred@numago.nl>
2025-03-03 19:56:25 +0100

    chore: Migrate from tailwind.config.ts to css based config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,26 +4,6 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 const config = {
 	darkMode: ['variant', ['&:where(.dark, .dark *)', '&:where(:global(html.dark-mode))']],
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				marker: ['"Permanent Marker"', ...defaultTheme.fontFamily.sans]
-			},
-			colors: {
-				assemble: {
-					light: '#fff0e8',
-					DEFAULT: '#ff9e64',
-					dark: '#471b00'
-				},
-				generate: {
-					light: '#f0ebf9',
-					DEFAULT: '#9d7cd8',
-					dark: '#1d1034'
-				}
-			}
-		}
-	},
-	plugins: [require('@tailwindcss/forms')]
 } satisfies Config
 
 export default config 
