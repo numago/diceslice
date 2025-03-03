dad1d9e27ec7d9bf3c90b9a903e2b4da101a2804
Fred Okoro <fred@numago.nl>
2025-03-03 20:00:47 +0100

    chore: Remove tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss'
-import defaultTheme from 'tailwindcss/defaultTheme'
-
-const config = {
-	darkMode: ['variant', ['&:where(.dark, .dark *)', '&:where(:global(html.dark-mode))']],
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-} satisfies Config
-
-export default config 
